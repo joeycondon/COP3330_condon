255f5cd0684bd815af58cd6bd44138807336c979
Joseph Condon <joeycondon@knights.ucf.edu>
2020-09-13 21:56:17 -0400

    updated homework

--- condon_p1/src/Decrypter.java
+++ condon_p1/src/Decrypter.java
@@ -1,6 +1,44 @@
+import java.util.Scanner;
+
 public class Decrypter {
-    public void main(){
-        
+    public static void main(String[] args) {
+        System.out.println("Please enter your data to be decrypted.");
+        Scanner input = new Scanner(System.in);
+        String data = input.next();
+        decrypt(data);
+        System.out.println("The data is now " + data);
     }
 
+    public static String decrypt(String data) {
+        int num = Integer.parseInt(data);
+        while (num > 1) {
+            int num4 = num % 100;
+            num /= 10;
+            int num3 = num % 100;
+            num /= 10;
+            int num2 = num % 100;
+            num /= 10;
+            int num1 = num % 100;
+            num /= 10;
+
+            num1 = (num1 + 7) % 10;
+            num2 = (num2 - 7) % 10;
+            num3 = (num3 - 7) % 10;
+            num4 = (num4 - 7) % 10;
+
+            int temp = 0;
+            temp = num1;
+            num1 = num3;
+            num3 = temp;
+            temp = num2;
+            num2 = num4;
+            num4 = temp;
+
+            String x = "" + num1 + num2 + num3 + num4;
+            data = x;
+
+            System.out.println(data);
+        }
+        return data;
+    }
 }
--- condon_p1/src/Encrypter.java
+++ condon_p1/src/Encrypter.java
@@ -1,5 +1,44 @@
+import java.util.Arrays;
+import java.util.Scanner;
 public class Encrypter {
-    public static void main(123){
+    /* Test the function*/
+    public static void main(String[] args) {
+        System.out.println("Please enter your data to be encrypted.");
+        Scanner input = new Scanner(System.in);
+        String data = input.next();
+        encrypt(data);
+    }
 
+    public static String encrypt(String data) {
+        int num = Integer.parseInt(data);
+        while (num > 1) {
+            int num4 = num % 100;
+            num /= 10;
+            int num3 = num % 100;
+            num /= 10;
+            int num2 = num % 100;
+            num /= 10;
+            int num1 = num % 100;
+            num /= 10;
+
+            num1 = (num1 + 7) % 10;
+            num2 = (num2 + 7) % 10;
+            num3 = (num3 + 7) % 10;
+            num4 = (num4 + 7) % 10;
+
+            int temp = 0;
+            temp = num1;
+            num1 = num3;
+            num3 = temp;
+            temp = num2;
+            num2 = num4;
+            num4 = temp;
+
+            String x = "" + num1 + num2 + num3 + num4;
+            data =  x;
+            System.out.println(data);
+            return data;
+        }
+        return data;
     }
 }
